e4de1512da71f9f2bc3fb0c3f6e318bc67814163
Duane Winner <duane@duanewinner.net>
2016-02-07 21:32:43 -0500

    burn to disc

--- flac_md5_validate.py
+++ flac_md5_validate.py
@@ -1,6 +1,7 @@
 #!/usr/local/bin/python3
 
 import hashlib
+import mimetypes
 import os
 import sys
 from collections import OrderedDict
@@ -16,7 +17,8 @@ def read_textfile_as_param():
         try:
             openfile = open(filename, 'r')
             try:
-                return openfile.read()
+                directory = os.path.dirname(sys.argv[1])
+                return (openfile.read(), directory)
             except UnicodeDecodeError:
                 print("\nFile \"{0}\" is not a text file.\n".format(filename))
                 sys.exit(1)
@@ -34,29 +36,64 @@ def read_textfile_as_param():
         print("\nUsage: ./final.py [path-to/]<filename>\n")
         sys.exit(1)
 
+def validate_manifest(manifest_file, manifest_directory):
+    manifest = []
+    for track in manifest_file.splitlines():
+        manifest.append((track.split()[1].replace('*',''),track.split()[0]))
 
-directory = os.path.dirname(sys.argv[1])
+    errors = False
+    for track in manifest:
+        try:
+            md5sum = hashlib.md5(open(manifest_directory + "/" + track[0],'rb').read()).hexdigest()
+        except FileNotFoundError:
+            print("{0} is missing!".format(track[0]))
+            errors = True
+        else:
+            if md5sum == track[1]:
+                print("{0} passes, ready to burn".format(track[0]))
+            else:
+                print("{0} md5 checksum does not match!".format(track[0]))
+                errors = True
 
-manifest = []
-for track in read_textfile_as_param().splitlines():
-	manifest.append((track.split()[1].replace('*',''),track.split()[0]))
+    if errors == False:
+        return(("-" * 50 + "\n" + "No errors found; ready to burn!\n" + "-" * 50), 0)
+    else:
+        return(("-" * 50 + "\n" + "Errors found!\n" + "-" * 50), 1)
+
+def check_for_flac():
+    return(os.path.isfile("/usr/local/bin/flac"))
+
+def burn_to_disc():
+    discs=set()
+    for line in file[0].splitlines():
+        discs.update(line[:-8][-1])
+    print("You will need {0} blank CD-R discs.".format(max(discs)))
+    while True:
+        confirm = input("Enter 'c' to continue, or 'q' to quit: ")
+        if confirm.lower() == 'q':
+            exit(0)
+        elif confirm.lower() == 'c':
+            for disc in sorted(discs):
+                print(disc)
+            exit(0)
 
 
-errors = False
-for track in manifest:
-	try:
-		md5sum = hashlib.md5(open(directory + "/" + track[0],'rb').read()).hexdigest()
-	except FileNotFoundError:
-		print("{0} is missing!".format(track[0]))
-		errors = True
-	else:
-		if md5sum == track[1]:
-			print("{0} passes, ready to burn".format(track[0]))
-		else:
-			print("{0} md5 checksum does not match!".format(track[0]))
-			errors = True
-if errors == False:
-	print("-" * 50 + "\n" + "No errors found; ready to burn!")
-else:
-	print("-" * 50 + "\n" + "Errors found!")
+if __name__ == '__main__':
+    file = read_textfile_as_param()
+    validation_results = (validate_manifest(file[0], file[1]))
+    print(validation_results[0])
+
+    if validation_results[1] == 1:
+        exit(1)
+    else:
+        if check_for_flac():
+            print("\"flac\" appears to be installed.")
+            while True:
+                burn_confim = input("Would you like to burn now with flac and drutil? (y/n): ")
+                if burn_confim.lower() == 'n':
+                    exit(0)
+                elif burn_confim.lower() == 'y':
+                    burn_to_disc()
+                    exit(0)
+
 
